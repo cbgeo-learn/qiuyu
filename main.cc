8106fbe3cfa01f32ff5adddcf9ab0e058274e49a
Amber1995 <wangqiuyu@utexas.edu>
2019-09-13 16:35:02 -0500

    Generate 100 particle sizes in RANDOM

--- main.cc
+++ main.cc
@@ -1,1 +1,12 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h> 
+
+int main(void)
+{
+srand((unsigned)time(NULL)); 
+for(int i = 0; i < 100;i++ ) 
+    std::cout << rand() / double(RAND_MAX) << std::endl;
+return 0;
+}
+
