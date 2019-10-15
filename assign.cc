3dd45d169a623cf3b4ec0f3fb0762dbb5670ee7e
Amber1995 <54647053+Amber1995@users.noreply.github.com>
2019-10-15 10:58:36 -0500

    Update assign.cc

--- assign.cc
+++ assign.cc
@@ -8,34 +8,35 @@ int main() {
 
   std::ifstream infile("radius.txt");
   std::ofstream ofs("assign100.txt");
-  int rows=100;
-  int cols=100;
-  int number=rows*cols;
+  int rows = 100;
+  int cols = 100;
+  int number = rows * cols;
   double radius[number] = {0};
   double xpost[number] = {0};
   double ypost[number] = {0};
   std::string line;
   int i = 0;
-  while(i<number)
-  if (infile.is_open())
-  {  while (std::getline(infile, line)) {
-      if (line != "") {
-        std::istringstream istream(line);
-        istream >> radius[i];
-        i = i + 1;
+  
+    if (infile.is_open()) 
+      while (std::getline(infile, line)) {
+        if (line != "") {
+          std::istringstream istream(line);
+          istream >> radius[i];
+          i = i + 1;
+         if(i>number)
+           break;
+        }
       }
-    }
-  }
   infile.close();
 
-  for (int i = 0; i < number; i=i+rows)
-  for (int j = 1; j <cols ; ++j)
-  xpost[i+j] = xpost[j-1] + radius[j] + radius[j-1];
- 
+  for (int i = 0; i < number; i = i + cols)
+    for (int j = 1; j < cols; ++j)
+      xpost[i + j] = xpost[j - 1] + radius[j] + radius[j - 1];
 
-  for(int i=rows;i<number;i=i+rows)
-    for(int j=0;j<cols;++j)
-  ypost[i+j] = ypost[i+j-rows] + radius[i+j-rows] + 2 * radius[i + j];
+  for (int i = cols; i < number; i = i + cols)
+    for (int j = 0; j < cols; ++j)
+      ypost[i + j] =
+          ypost[i + j - cols] + radius[i + j - cols] + 2 * radius[i + j];
 
   for (int i = 0; i < number; ++i)
     if (ofs.is_open()) {
@@ -43,4 +44,5 @@ int main() {
       ofs << radius[i] << " " << xpost[i] << " " << ypost[i] << "\n";
     }
   ofs.close();
-}
+
+    }
